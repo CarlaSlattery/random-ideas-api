75b614f2ffc7ba2001f63ae02f91f9e0bce6555f
CarlaSlattery <cslatterydev@gmail.com>
2023-08-04 10:12:31 +0100

    update and delete requests working

--- routes/ideas.js
+++ routes/ideas.js
@@ -34,25 +34,51 @@ router.get("/", (req, res) => {
 router.get("/:id", (req, res) => {
   const idea = ideas.find((idea) => idea.id === +req.params.id);
 
-  if(!idea){
-   return res.status(404).json({success:false, error: "Resource not found"})
+  if (!idea) {
+    return res
+      .status(404)
+      .json({ success: false, error: "Resource not found" });
   }
   res.json({ success: true, data: idea });
 });
 
 //Add an idea
 router.post("/", (req, res) => {
-    const idea = {
-        id: ideas.length + 1,
-        text: req.body.text,
-        tag: req.body.tag,
-        username: req.body.username,
-        date: new Date().toISOString().slice(0,10),
-    }
-    ideas.push(idea)
-    res.json({success: true, data: idea})
+  const idea = {
+    id: ideas.length + 1,
+    text: req.body.text,
+    tag: req.body.tag,
+    username: req.body.username,
+    date: new Date().toISOString().slice(0, 10),
+  };
+  ideas.push(idea);
+  res.json({ success: true, data: idea });
+});
 
- })
+//Update idea
+router.put("/:id", (req, res) => {
+  const idea = ideas.find((idea) => idea.id === +req.params.id);
 
+  if (!idea) {
+    res.status(404).json({ success: false, error: "Resource not found" });
+  } else {
+    idea.text = req.body.text || idea.text;
+    idea.tag = req.body.tag || idea.tag;
+
+    res.json({ success: true, data: idea });
+  }
+});
+
+//delete an idea
+router.delete("/:id", (req, res) => {
+  const idea = ideas.find((idea) => idea.id === +req.params.id);
+  if (!idea) {
+    res.status(404).json({ success: false, error: "Resource not found" });
+  } else {
+    const index = ideas.indexOf(idea)
+    ideas.splice(index, 1);
+    res.json({ success: true, data: {} });
+  }
+});
 
 module.exports = router;
